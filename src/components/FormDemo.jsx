ef2ca7468258ad55c3c70befc9b6993913f8baa5
dfldiego <dfl.diego92@gmail.com>
2021-07-31 12:10:37 -0300

    validacion de checkbox, textarea, errorMessage

--- src/components/FormDemo.jsx
+++ src/components/FormDemo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {
   Box,
+  Button,
   Card,
   CardContent,
   Checkbox,
@@ -10,14 +11,14 @@ import {
   TextField,
   Typography,
 } from "@material-ui/core";
-import { Field, Formik, Form, useField } from "formik";
-import { boolean, number, object, string } from "yup";
+import { Field, Formik, Form, useField, ErrorMessage } from "formik";
+import { array, boolean, mixed, number, object, string } from "yup";
 /* import InvestmentDetails from "./InvestmentDetails"; */
 
 // valores iniciales (fuera del componente)
 const initialValues = {
   fullName: "",
-  initialInvestment: undefined,
+  initialInvestment: 0,
   investmentRisk: [],
   commentAboutInvestmentRisk: "",
   dependents: -1,
@@ -32,10 +33,22 @@ const FormDemo = () => {
 
         <Formik
           validationSchema={object({
-            fullName: string().required().min(2).max(100),
+            fullName: string()
+              .required("Your name is mandatory")
+              .min(2)
+              .max(100),
             initialInvestment: number().required().min(2),
             dependents: number().required().min(0).max(5),
             acceptedTermsAndConditions: boolean().oneOf([true]),
+            investmentRisk: array(
+              string().oneOf(["High", "Medium", "Low"])
+            ).min(1),
+            commentAboutInvestmentRisk: mixed().when("investmentRisk", {
+              is: (investmentRisk) =>
+                investmentRisk.find((ir) => ir === "High"),
+              then: string().required().min(20).max(100),
+              otherwise: string().min(28).max(100),
+            }),
           })}
           initialValues={initialValues}
           onSubmit={() => {}}
@@ -45,6 +58,7 @@ const FormDemo = () => {
               <Box mb={2}>
                 <FormGroup>
                   <Field name="fullName" as={TextField} label="Full Name" />
+                  <ErrorMessage name="fullName" />
                 </FormGroup>
               </Box>
               <Box mb={2}>
@@ -55,6 +69,7 @@ const FormDemo = () => {
                     as={TextField}
                     label="Initial Investment"
                   />
+                  <ErrorMessage name="initialInvestment" />
                 </FormGroup>
               </Box>
               <Box mb={2}>
@@ -74,7 +89,13 @@ const FormDemo = () => {
                     value="Low"
                     label="Low - Safe"
                   />
+                  <MyCheckbox
+                    name="investmentRisk"
+                    value="very Low"
+                    label="very Low"
+                  />
                 </FormGroup>
+                <ErrorMessage name="investmentRisk" />
               </Box>
               <Box mb={2}>
                 <FormGroup>
@@ -85,11 +106,15 @@ const FormDemo = () => {
                     rows={3}
                     rowsMax={10}
                   />
+                  <ErrorMessage name="commentAboutInvestmentRisk" />
                 </FormGroup>
               </Box>
               <Box mb={2}>
                 <FormGroup>
                   <Field name="dependents" as={TextField} select>
+                    <MenuItem value={-1} disabled>
+                      --Select one dependents--
+                    </MenuItem>
                     <MenuItem value={0}>0</MenuItem>
                     <MenuItem value={1}>1</MenuItem>
                     <MenuItem value={2}>2</MenuItem>
@@ -97,6 +122,7 @@ const FormDemo = () => {
                     <MenuItem value={4}>4</MenuItem>
                     <MenuItem value={5}>5</MenuItem>
                   </Field>
+                  <ErrorMessage name="dependents" />
                 </FormGroup>
               </Box>
               <Box mb={2}>
@@ -105,7 +131,11 @@ const FormDemo = () => {
                     name="acceptedTermsAndConditions"
                     label="Accepted terms and conditions"
                   />
+                  <ErrorMessage name="acceptedTermsAndConditions" />
                 </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <Button variant="contained">Submit</Button>
               </Box>
               <pre>{JSON.stringify(errors, null, 4)}</pre>
               <pre>{JSON.stringify(values, null, 4)}</pre>
