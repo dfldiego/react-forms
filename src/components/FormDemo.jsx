7288047bcb42fa03a6669117e8d634499af97867
dfldiego <dfl.diego92@gmail.com>
2021-07-30 21:12:22 -0300

    estilos al formulario con material UI

--- src/components/FormDemo.jsx
+++ src/components/FormDemo.jsx
@@ -1,6 +1,16 @@
 import React from "react";
-import { Card, CardContent, Typography } from "@material-ui/core";
-import { Field, Formik, Form } from "formik";
+import {
+  Box,
+  Card,
+  CardContent,
+  Checkbox,
+  FormControlLabel,
+  FormGroup,
+  MenuItem,
+  TextField,
+  Typography,
+} from "@material-ui/core";
+import { Field, Formik, Form, useField } from "formik";
 /* import InvestmentDetails from "./InvestmentDetails"; */
 
 // valores iniciales (fuera del componente)
@@ -22,26 +32,71 @@ const FormDemo = () => {
         <Formik initialValues={initialValues} onSubmit={() => {}}>
           {({ values }) => (
             <Form>
-              <Field name="fullName" />
-              <Field name="initialInvestment" type="number" />
-
-              <Field name="investmentRisk" type="checkbox" value="High" />
-              <Field name="investmentRisk" type="checkbox" value="Medium" />
-              <Field name="investmentRisk" type="checkbox" value="Low" />
-
-              <Field name="commentAboutInvestmentRisk" as="textarea" />
-
-              <Field name="dependents" as="select">
-                <option value={0}>0</option>
-                <option value={1}>1</option>
-                <option value={2}>2</option>
-                <option value={3}>3</option>
-                <option value={4}>4</option>
-                <option value={5}>5</option>
-              </Field>
-
-              <Field name="acceptedTermsAndConditions" type="checkbox" />
-
+              <Box mb={2}>
+                <FormGroup>
+                  <Field name="fullName" as={TextField} label="Full Name" />
+                </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <FormGroup>
+                  <Field
+                    name="initialInvestment"
+                    type="number"
+                    as={TextField}
+                    label="Initial Investment"
+                  />
+                </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <FormGroup>
+                  <MyCheckbox
+                    name="investmentRisk"
+                    value="High"
+                    label="High - Super Risky"
+                  />
+                  <MyCheckbox
+                    name="investmentRisk"
+                    value="Medium"
+                    label="Medium - Risky"
+                  />
+                  <MyCheckbox
+                    name="investmentRisk"
+                    value="Low"
+                    label="Low - Safe"
+                  />
+                </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <FormGroup>
+                  <Field
+                    name="commentAboutInvestmentRisk"
+                    as={TextField}
+                    multiline
+                    rows={3}
+                    rowsMax={10}
+                  />
+                </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <FormGroup>
+                  <Field name="dependents" as={TextField} select>
+                    <MenuItem value={0}>0</MenuItem>
+                    <MenuItem value={1}>1</MenuItem>
+                    <MenuItem value={2}>2</MenuItem>
+                    <MenuItem value={3}>3</MenuItem>
+                    <MenuItem value={4}>4</MenuItem>
+                    <MenuItem value={5}>5</MenuItem>
+                  </Field>
+                </FormGroup>
+              </Box>
+              <Box mb={2}>
+                <FormGroup>
+                  <MyCheckbox
+                    name="acceptedTermsAndConditions"
+                    label="Accepted terms and conditions"
+                  />
+                </FormGroup>
+              </Box>
               <pre>{JSON.stringify(values, null, 4)}</pre>
             </Form>
           )}
@@ -52,3 +107,17 @@ const FormDemo = () => {
 };
 
 export default FormDemo;
+
+export function MyCheckbox(props) {
+  const [field] = useField({
+    name: props.name,
+    type: "checkbox",
+    value: props.value,
+  });
+  return (
+    <FormControlLabel
+      control={<Checkbox {...props} {...field} />}
+      label={props.label}
+    />
+  );
+}
